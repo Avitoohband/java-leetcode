5733a3de0fff45ed7d33eab1ef291bcdd14cc0e3
Avi Tuchband <avitoohband@gmail.com>
2024-01-20 17:33:15 +0200

    queens 2 progress

---
+++ src/nqueens2/NQueens2.java
@@ -0,0 +1,28 @@
+package src.nqueens2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NQueens2 {
+    public static void main(String[] args) {
+        int n = 4;
+
+    }
+    public static int totalNQueens(int n){
+        Set<Integer> pos = new HashSet<>();
+        Set<Integer> neg = new HashSet<>();
+        Set<Integer> col = new HashSet<>();
+
+        int[][] board = new int[n][n];
+
+    }
+
+    public static int solve(int n, int currentQueens,Set<Integer> pos,
+                            Set<Integer> neg, Set<Integer> col){
+        if(currentQueens == n) return 1;
+
+        for
+
+    }
+
+}
