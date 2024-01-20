2d9f8dfa0c4baab59ea2d6b69d77ebdc97131f00
Avi Tuchband <avitoohband@gmail.com>
2024-01-20 12:42:25 +0200

    backtracking started -> letter combinations of a phone

--- src/lettercombinationsofaphonenumber/LetterCombinationsOfAPhoneNumber.java
+++ src/lettercombinationsofaphonenumber/LetterCombinationsOfAPhoneNumber.java
@@ -5,25 +5,33 @@ import java.util.*;
 public class LetterCombinationsOfAPhoneNumber {
     public static void main(String[] args) {
 
+        System.out.println(letterCombinations("23"));
+
     }
-    public static List<String> letterCombinations (String digits){
-        Map<Integer, List<Character>> numbersToLetterMap = new HashMap<>();
-        numbersToLetterMap.put(2, List.of('a', 'b', 'c'));
-        numbersToLetterMap.put(3, List.of('d', 'e', 'f'));
-        numbersToLetterMap.put(4, List.of('g', 'h', 'i'));
-        numbersToLetterMap.put(5, List.of('j', 'k', 'l'));
-        numbersToLetterMap.put(6, List.of('m', 'n', 'o'));
-        numbersToLetterMap.put(7, List.of('p', 'q', 'r', 's'));
-        numbersToLetterMap.put(8, List.of('t', 'u', 'v'));
-        numbersToLetterMap.put(9, List.of('w', 'x', 'y', 'z'));
 
-        List<String> solution = new ArrayList<>();
-        //for every letter in digits, add all possible solutions, when reached last digits , add the sol to array
+    public static List<String> letterCombinations(String digits) {
+        if (digits.length() < 1) return List.of(digits);
 
-        if(digits.length() < 1 ) return Collections.emptyList();
 
-        for (Character c : numbersToLetterMap.get(digits.toCharArray()[0])) {
-            buildString()
+        String[] keypad = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> solutions = new ArrayList<>();
+
+        solve(solutions, "", keypad, digits, 0);
+
+        return solutions;
+
+    }
+
+    public static void solve(List<String> solutions, String currentComb,
+                             String[] keypad, String digits, int digitsIndex) {
+
+        if (currentComb.length() == digits.length()) {
+            solutions.add(currentComb);
+        } else {
+            for (Character c : keypad[digits.charAt(digitsIndex) - '2'].toCharArray()) {
+                solve(solutions, currentComb + c, keypad, digits, digitsIndex + 1);
+            }
         }
     }
+
 }
