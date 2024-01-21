2f15d7b3e1f8d1e317de05d33f1ce22f08076f84
Avi Tuchband <avitoohband@gmail.com>
2024-01-21 15:43:17 +0200

    finished nqeens 2

--- src/nqueens2/NQueens2.java
+++ src/nqueens2/NQueens2.java
@@ -7,22 +7,50 @@ public class NQueens2 {
     public static void main(String[] args) {
         int n = 4;
 
+        System.out.println(totalNQueens(n));
+
     }
-    public static int totalNQueens(int n){
+
+    public static int totalNQueens(int n) {
         Set<Integer> pos = new HashSet<>();
         Set<Integer> neg = new HashSet<>();
         Set<Integer> col = new HashSet<>();
+        Set<Integer> row = new HashSet<>();
 
-        int[][] board = new int[n][n];
+        return solve(n, 0, pos, neg, col, row);
 
     }
 
-    public static int solve(int n, int currentQueens,Set<Integer> pos,
-                            Set<Integer> neg, Set<Integer> col){
-        if(currentQueens == n) return 1;
+    public static int solve(int n, int numberOfQueens, Set<Integer> pos,
+                            Set<Integer> neg, Set<Integer> col, Set<Integer> row) {
+        if (numberOfQueens == n) {
+            return 1;
+        }
+        ;
 
-        for
+        int numberOfWays = 0;
+        for (int i = 0; i < n; i++) {
+            if (
+                    !col.contains(i) &&
+                            !row.contains(numberOfQueens) &&
+                            !pos.contains(numberOfQueens + i) &&
+                            !neg.contains(numberOfQueens - i)) {
+                col.add(i);
+                row.add(numberOfQueens);
+                pos.add(numberOfQueens + i);
+                neg.add(numberOfQueens - i);
+                numberOfWays += solve(n, numberOfQueens + 1, pos, neg, col, row);
 
+                col.remove(i);
+                row.remove(numberOfQueens);
+                pos.remove(numberOfQueens + i);
+                neg.remove(numberOfQueens - i);
+            }
+
+        }
+        return numberOfWays;
     }
 
 }
+
+
