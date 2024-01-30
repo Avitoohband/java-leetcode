59ed78525071371444f67592813e89c6903e1997
Avi Tuchband <avitoohband@gmail.com>
2024-01-30 14:10:57 +0200

    30-01-2024

---
+++ src/reversewordsinastring/ReverseWordsString.java
@@ -0,0 +1,33 @@
+package src.reversewordsinastring;
+
+public class ReverseWordsString {
+
+    public static void main(String[] args) {
+
+        String s = "the sky is blue";
+        //Output: "blue is sky the"
+        System.out.println(reverseWords(s));
+
+    }
+
+    public static String reverseWords(String s){
+            if(s.length() < 2) return s;
+            StringBuilder sb = new StringBuilder();
+
+            for(int startWord  = s.length() - 1 ; startWord >= 0 ; startWord--){
+
+                if(s.charAt(startWord) == ' ') continue;
+
+                int endWords = startWord;
+
+                while(endWords >= 0 && s.charAt(endWords) != ' '){
+                    endWords--;
+                }
+
+                if(!sb.isEmpty()) sb.append(' ');
+                sb.append(s, endWords + 1, startWord + 1);
+                startWord = endWords;
+            }
+            return sb.toString();
+        }
+}
