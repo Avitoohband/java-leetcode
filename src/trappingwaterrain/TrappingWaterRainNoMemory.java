2be0e613b1dc6c037c914aa657fe2a05050a72b8
Avi Tuchband <avitoohband@gmail.com>
2024-12-20 14:25:58 +0200

    TrappingWaterRainNoMemory

--- src/trappingwaterrain/TrappingWaterRainNoMemory.java
+++ src/trappingwaterrain/TrappingWaterRainNoMemory.java
@@ -2,11 +2,28 @@ package src.trappingwaterrain;
 
 public class TrappingWaterRainNoMemory {
     public static void main(String[] args) {
+        int[] water = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trap(water));
 
     }
 
     public static int trap(int[] height) {
-      return 0;
+        int left = 0;
+        int right = height.length - 1;
+        int maxLeft = 0;
+        int maxRight = 0;
+        int trappedWater = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]){
+                maxLeft = Math.max(height[left],maxLeft);
+                trappedWater += Math.max(maxLeft - height[left++], 0);
+            }else {
+                maxRight = Math.max(height[right],maxRight);
+                trappedWater += Math.max(maxRight - height[right--], 0);
+            }
+        }
+        return trappedWater;
     }
 }
 
