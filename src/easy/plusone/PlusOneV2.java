077d2dc9d6db3c92561889301f7b14d0b4d1ed36
Avi Tuchband <Avitoohband@gmail.com>
2024-12-21 21:14:16 +0200

    PlusOneV2

--- src/easy/plusone/PlusOneV2.java
+++ src/easy/plusone/PlusOneV2.java
@@ -1,25 +1,22 @@
 package src.easy.plusone;
 
+import javax.swing.plaf.synth.SynthStyleFactory;
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
-public class PlusOne {
-    public static int[] plusOne(int[] digits){
-        int end;
-        for (end = digits.length - 1; end >= 0; end--) {
-            if(digits[end] == 9) {
-                digits[end] = 0;
-                continue;
+public class PlusOneV2 {
+    public static int[] plusOne(int[] digits) {
+        int index = digits.length - 1;
+        while (index >= 0) {
+            if (digits[index] < 9) {
+                digits[index]++;
+                return digits;
             }
-            digits[end]++;
-            return digits;
+            digits[index--] = 0;
         }
-
-        int[] newDigits = new int[digits.length + 1];
-        newDigits[0] = 1;
-
-        System.arraycopy(digits, 0, newDigits, 1, newDigits.length - 1);
-
-        return newDigits;
-
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
     }
 }
